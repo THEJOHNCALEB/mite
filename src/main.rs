6968e5960036ed9cf78d06c58548f9fa70f23311
John Caleb <Calebjohn3112@gmail.com>
2025-01-08 09:10:21 +0100

    ready to publish

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Mite {
     command: String,
-    option: std::path::PathBuf,
+    option: PathBuf,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let mite_stuff = Mite::parse();
-    let content = std::fs::read_to_string(&mite_stuff.option).expect("could not read file");
-    for word in content.lines() {
-        if word.contains(&mite_stuff.command) {
-            println!("{}", word);
+    let file = File::open(&mite_stuff.option).expect("could not open file");
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.contains(&mite_stuff.command) {
+            println!("{}", line);
         }
     }
     println!("{:?} {:?}", mite_stuff.command, mite_stuff.option);
+    Ok(())
 }
